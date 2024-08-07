2e79a9dcc980a5dc85ed0a88197add37a8838a73
yzx9 <yuan.zx@outlook.com>
2024-08-07 16:22:09 +0800

    feat: add command to toggle format-on-save

--- home/neovim/plugins/conform.nix
+++ home/neovim/plugins/conform.nix
@@ -1,67 +1,76 @@
 # Lightweight yet powerful formatter plugin for Neovim
 # homepage: https://github.com/stevearc/conform.nvim
 # nixvim doc: https://nix-community.github.io/nixvim/plugins/conform-nvim/index.html
-{ ... }:
+{ lib, ... }:
 
 {
-  programs.nixvim.plugins.conform-nvim = {
-    enable = true;
-    formatOnSave = {
-      lspFallback = true;
-      timeoutMs = 500;
+  programs.nixvim = {
+    plugins.conform-nvim = {
+      enable = true;
+      notifyOnError = true;
+      formattersByFt =
+        let
+          prettier = [
+            "prettierd"
+            "prettier"
+          ];
+
+          genPretiier = langs: lib.genAttrs langs (lang: [ prettier ]);
+        in
+        genPretiier [
+          "html"
+          "css"
+          "javascript"
+          "typescript"
+          "vue"
+          "markdown"
+        ]
+        // {
+          python = [
+            "isort"
+            "black"
+          ];
+          rust = [ "rustfmt" ];
+          nix = [ "nixfmt" ];
+          go = [
+            "goimports"
+            "gofmt"
+          ];
+          yaml = [
+            "yamllint"
+            "yamlfmt"
+          ];
+        };
+
+      formatOnSave = ''
+        function(bufnr)
+          -- Disable with a global or buffer-local variable
+          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+          end
+          return { timeout_ms = 500, lsp_format = "fallback" }
+        end
+      '';
     };
-    notifyOnError = true;
-    formattersByFt = {
-      html = [
-        [
-          "prettierd"
-          "prettier"
-        ]
-      ];
-      css = [
-        [
-          "prettierd"
-          "prettier"
-        ]
-      ];
-      javascript = [
-        [
-          "prettierd"
-          "prettier"
-        ]
-      ];
-      typescript = [
-        [
-          "prettierd"
-          "prettier"
-        ]
-      ];
-      vue = [
-        [
-          "prettierd"
-          "prettier"
-        ]
-      ];
-      markdown = [
-        [
-          "prettierd"
-          "prettier"
-        ]
-      ];
-      python = [
-        "isort"
-        "black"
-      ];
-      rust = [ "rustfmt" ];
-      nix = [ "nixfmt" ];
-      go = [
-        "goimports"
-        "gofmt"
-      ];
-      yaml = [
-        "yamllint"
-        "yamlfmt"
-      ];
-    };
+
+    extraConfigLua = ''
+      vim.api.nvim_create_user_command("FormatDisable", function(args)
+        if args.bang then
+          -- FormatDisable! will disable formatting just for this buffer
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
+        end
+      end, {
+        desc = "Disable autoformat-on-save",
+        bang = true,
+      })
+      vim.api.nvim_create_user_command("FormatEnable", function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+      end, {
+        desc = "Re-enable autoformat-on-save",
+      })
+    '';
   };
 }
