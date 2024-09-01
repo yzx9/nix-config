28fe6f83418a05f964661f411956b51d6ad99eb0
yzx9 <yuan.zx@outlook.com>
2024-09-01 20:00:59 +0800

    feat: custom NIX_PATH and flake registry

--- modules/common/nix-core.nix
+++ modules/common/nix-core.nix
@@ -1,4 +1,5 @@
 {
+  inputs,
   vars,
   pkgs,
   lib,
@@ -6,14 +7,26 @@
 }:
 
 {
-  nix.settings = {
-    # enable flakes globally
-    experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
+  # Flake
+  # enable flakes globally
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
-    # substituers that will be considered before the official ones(https://cache.nixos.org)
+  # Custom NIX_PATH and flake registry
+  # https://nixos-and-flakes.thiscute.world/best-practices/nix-path-and-flake-registry
+
+  # make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
+  nix.registry.nixpkgs.flake = inputs.nixpkgs;
+  nix.channel.enable = false; # remove nix-channel related tools & configs, we use flakes instead.
+
+  # but NIX_PATH is still used by many useful tools, so we set it to the same value as the one used by this flake.
+  # Make `nix repl '<nixpkgs>'` use the same nixpkgs as the one used by this flake.
+  environment.etc."nix/inputs/nixpkgs".source = "${inputs.nixpkgs}";
+
+  nix.settings = {
+    # substituers that will be considered before the official ones (https://cache.nixos.org)
     substituters = [
       # "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://nix-community.cachix.org"
