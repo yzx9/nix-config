5e6a1240b87bf7fb50461538f5ceaab886c4eda3
yzx9 <yuan.zx@outlook.com>
2024-09-03 10:26:22 +0800

    feat(home/gui): always add kitty terminfo

--- home/gui/kitty.nix
+++ home/gui/kitty.nix
@@ -1,24 +1,33 @@
-{ config, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 let
   cfg = config.gui;
 in
 {
-  config = lib.mkIf cfg.enable {
-    programs.kitty = {
-      enable = true;
-      font = {
-        name = "FiraCode Nerd Font";
+  config =
+    lib.mkIf cfg.enable {
+      programs.kitty = {
+        enable = true;
+        font = {
+          name = "FiraCode Nerd Font";
+        };
+        extraConfig = ''
+          background_opacity 0.75
+          macos_show_window_title_in window
+
+          # Note that disabling the read confirmation is this akes a security risk
+          clipboard_control write-clipboard write-primary read-clipboard read-primary
+        '';
       };
-      extraConfig = ''
-        background_opacity 0.75
-        macos_show_window_title_in window
 
-        # Note that disabling the read confirmation is this akes a security risk
-        clipboard_control write-clipboard write-primary read-clipboard read-primary
-      '';
+      home.shellAliases.s = "kitten ssh";
+    }
+    // {
+      home.packages = [ pkgs.kitty.terminfo ];
     };
-
-    home.shellAliases.s = "kitten ssh";
-  };
 }
