415667df79b5b06b4b8a4881c4f467116b7833e3
yzx9 <git@yzx9.xyz>
2024-09-13 14:01:40 +0800

    refactor: update secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,10 @@
 let
   user_yzx9-mbp = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH8z6g8A53hF7ATKNIGzbYf/rhSm6z+iyPm2nn7E4bnp yzx9@yzx9-mbp";
   user_yzx9-ws = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGN854HSrlbBR4/yB0sRk9Plerh2rKn3ZqCZJULRRB8S yzx9@yzx9-ws";
-  admins = [ user_yzx9-mbp ];
+  users = [
+    user_yzx9-ws
+    user_yzx9-mbp
+  ];
 
   system_yzx9-mbp = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFJCN9HFXys1Ov8cCFHqpNJ61uVm642fPCVGVXqnw4KP";
   system_yzx9-ws = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDxJAOqmvqV1hO39/E7SCzHI3xwqHfhNt4MWXKYYZ12m root@nixos";
@@ -9,16 +12,19 @@ let
     system_yzx9-mbp
     system_yzx9-ws
   ];
+
+  yzx9-mbp = [ user_yzx9-mbp ] ++ [ system_yzx9-mbp ];
+  yzx9-ws = [ user_yzx9-ws ] ++ [ system_yzx9-ws ];
 in
 {
-  "ssh-config.age".publicKeys = admins ++ systems;
+  "ssh-config.age".publicKeys = users;
 
   # yzx9-mbp only
-  "id-git_yzx9-mbp.age".publicKeys = admins ++ [ system_yzx9-mbp ];
-  "id-github_yzx9-mbp.age".publicKeys = admins ++ [ system_yzx9-mbp ];
-  "ssh-config_yzx9-mbp.age".publicKeys = admins ++ [ system_yzx9-mbp ];
+  "id-git_yzx9-mbp.age".publicKeys = yzx9-mbp;
+  "id-github_yzx9-mbp.age".publicKeys = yzx9-mbp;
+  "ssh-config_yzx9-mbp.age".publicKeys = yzx9-mbp;
 
   # yzx9-ws only
-  "frpc_yzx9-ws.toml.age".publicKeys = admins ++ [ user_yzx9-ws ] ++ [ system_yzx9-ws ];
-  "xray_yzx9-ws.json.age".publicKeys = admins ++ [ user_yzx9-ws ] ++ [ system_yzx9-ws ];
+  "frpc_yzx9-ws.toml.age".publicKeys = yzx9-ws;
+  "xray_yzx9-ws.json.age".publicKeys = yzx9-ws;
 }
