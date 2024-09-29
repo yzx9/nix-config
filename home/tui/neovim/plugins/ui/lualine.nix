16c540ccfe1f4e1b5ee1dc59915fce5edca1006a
yzx9 <git@yzx9.xyz>
2024-09-29 21:54:34 +0800

    refactor(home/tui/neovim): bump lualine

--- home/tui/neovim/plugins/ui/lualine.nix
+++ home/tui/neovim/plugins/ui/lualine.nix
@@ -24,69 +24,55 @@ in
             bg = "#b4befe";
             fg = "#1c1d21";
           };
-          b = {
-            bg = "nil";
-          };
-          c = {
-            bg = "nil";
-          };
-          z = {
-            bg = "nil";
-          };
-          y = {
-            bg = "nil";
-          };
+          b.bg = "nil";
+          c.bg = "nil";
+          y.bg = "nil";
+          z.bg = "nil";
         };
       };
       sections = {
         lualine_a = [
           {
-            name = "mode";
+            __unkeyed-1 = "mode";
             fmt = "string.lower";
           }
         ];
         lualine_b = [
           {
-            name = "branch";
+            __unkeyed-1 = "branch";
             icon = icons.GitBranch; # 
           }
           "diff"
         ];
         lualine_c = [
           {
-            name = "diagnostic";
-            extraConfig = {
-              symbols = {
-                error = icons.DiagnosticError; # " "
-                warn = icons.DiagnosticWarn; # " "
-                info = icons.DiagnosticInfo; # " "
-                hint = icons.DiagnosticHint; # "󰝶 "
-              };
+            __unkeyed-1 = "diagnostic";
+            symbols = {
+              error = icons.DiagnosticError; # " "
+              warn = icons.DiagnosticWarn; # " "
+              info = icons.DiagnosticInfo; # " "
+              hint = icons.DiagnosticHint; # "󰝶 "
             };
           }
         ];
         lualine_x = [
           {
-            name = "filetype";
-            extraConfig = {
-              icon_only = true;
-            };
+            __unkeyed-1 = "filetype";
+            icon_only = true;
           }
         ];
         lualine_y = [
           {
-            name = "filename";
-            extraConfig = {
-              symbols = {
-                modified = icons.FileModified; # " "
-                readonly = "";
-                unnamed = "";
-              };
+            __unkeyed-1 = "filename";
+            symbols = {
+              modified = icons.FileModified; # " "
+              readonly = "";
+              unnamed = "";
             };
             separator.left = "";
           }
         ];
-        lualine_z = [ { name = "location"; } ];
+        lualine_z = [ "location" ];
       };
     };
   };
