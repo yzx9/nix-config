c974c9872fbbabeeae4f94c0fdaaab72ed4ac29c
Zexin Yuan <git@yzx9.xyz>
2024-12-07 00:51:24 +0800

    feat(hosts): update network

--- hosts/yzx9-rpi5/host.nix
+++ hosts/yzx9-rpi5/host.nix
@@ -1,17 +1,39 @@
 { lib, ... }:
 
 {
-  imports = [ ./rpi5.nix ];
+  imports = [
+    ./rpi5.nix
+    ./frpc.nix
+  ];
 
   # networking
-  networking.networkmanager.enable = lib.mkForce false; # we have to disable it since webgitkit cache-missing
-  networking.wireless = {
-    # Enables wireless support via wpa_supplicant.
-    enable = true;
-    networks = {
-      chn = {
-        pskRaw = "935490cd011d5c6af8fa1b12a2fce67437d6fcc800daf278b0e6342ca3e97374";
-      };
+  networking = {
+    networkmanager.enable = lib.mkForce false; # we have to disable it since webgitkit cache-missing
+
+    interfaces.end0 = {
+      ipv4.addresses = [
+        {
+          address = "10.6.18.188";
+          prefixLength = 24;
+        }
+      ];
     };
+
+    defaultGateway = {
+      address = "10.6.18.254";
+      interface = "end0";
+    };
+
+    nameservers = [ "10.6.31.1" ];
+
+    # wireless = {
+    #   # Enables wireless support via wpa_supplicant.
+    #   enable = true;
+    #   networks = {
+    #     chn = {
+    #       pskRaw = "935490cd011d5c6af8fa1b12a2fce67437d6fcc800daf278b0e6342ca3e97374";
+    #     };
+    #   };
+    # };
   };
 }
