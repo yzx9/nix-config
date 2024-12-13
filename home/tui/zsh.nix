da4505db2d0be1b8bb3efccca442aac5c866eff7
Zexin Yuan <git@yzx9.xyz>
2024-12-13 18:09:32 +0800

    fix(home/tui): supress zsh compinit warning

--- home/tui/zsh.nix
+++ home/tui/zsh.nix
@@ -1,11 +1,23 @@
-{ pkgs, ... }:
+{
+  vars,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
   programs.zsh = {
     enable = true;
+
     enableCompletion = true;
+    completionInit =
+      lib.optionalString (vars.type != "home-manager") "autoload -U compinit && compinit"
+      + lib.optionalString (vars.type == "home-manager") "autoload -U compinit && compinit -u";
+
     autosuggestion.enable = true;
+
     syntaxHighlighting.enable = true;
+
     plugins = [
       {
         name = "vi-mode";
