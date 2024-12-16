488a4c6516d9d89afb77c35b95e12dcc0387dd5a
Zexin Yuan <git@yzx9.xyz>
2024-12-16 17:09:42 +0800

    fix(home/gui): migrate nerf fonts

--- home/gui/fonts.nix
+++ home/gui/fonts.nix
@@ -13,11 +13,8 @@ in
     fonts.fontconfig.enable = true;
 
     home.packages = with pkgs; [
-      # It is sometimes useful to fine-tune packages, for example, by applying
-      # overrides. You can do that directly here, just don't forget the
-      # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-      # fonts?
-      (nerdfonts.override { fonts = [ "FiraCode" ]; })
+      # It is sometimes useful to fine-tune packages
+      nerd-fonts.fira-code
 
       cascadia-code # Cascadia Code NF added since 2404.23
 
