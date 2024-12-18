e2e6f92744e56a2ed1ca092695b227998b4a1783
Zexin Yuan <git@yzx9.xyz>
2024-12-18 15:19:44 +0800

    style: format

--- options.nix
+++ options.nix
@@ -13,28 +13,34 @@ in
       ];
       description = "The type of configuration to generate";
     };
+
     hostname = lib.mkOption {
       type = lib.types.str;
       description = "The hostname of the machine";
     };
+
     system = lib.mkOption {
       type = lib.types.enum [
         "x86_64-linux"
         "aarch64-linux"
         "aarch64-darwin"
       ];
+
       description = "The system of the machine";
     };
+
     user = {
       name = lib.mkOption {
         type = lib.types.str;
         description = "The name of the user";
       };
+
       git = {
         name = lib.mkOption {
           type = lib.types.str;
           description = "The name of the user";
         };
+
         email = lib.mkOption {
           type = lib.types.str;
           description = "The email of the user";
