1557cd6e4e9cf8307f230fc100067a03f3ac3545
Zexin Yuan <git@yzx9.xyz>
2024-12-19 11:01:30 +0800

    perf: remove duplicate option

--- modules/nix-darwin/system.nix
+++ modules/nix-darwin/system.nix
@@ -37,7 +37,6 @@ in
 
   # Create /etc/zshrc that loads the nix-darwin environment.
   # this is required if you want to use darwin's default shell - zsh
-  programs.zsh.enable = true;
   environment.shells = [ pkgs.zsh ];
   environment.pathsToLink = [ "/share/zsh" ];
 
