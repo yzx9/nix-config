63d8c4e3f4e2848acb1641a5590abf9d6a70e750
Zexin Yuan <git@yzx9.xyz>
2024-12-23 17:24:44 +0800

    docs: update notes for darwin

--- modules/_shared/apps.nix
+++ modules/_shared/apps.nix
@@ -2,17 +2,19 @@
 #
 #  Install all apps and packages here.
 #
-#  NOTE: Your can find all available options in:
+##########################################################################
+
+# Darwin
+#
+# NOTE: Your can find all available options in:
 #    https://daiderd.com/nix-darwin/manual/index.html
 #
-#  NOTE：To remove the uninstalled APPs icon from Launchpad:
-#    1. `sudo nix store gc --debug` & `sudo nix-collect-garbage --delete-old`
+# NOTE: To remove the uninstalled APPs icon from Launchpad:
+#    1. `sudo nix store gc --debug && sudo nix-collect-garbage --delete-old`
 #    2. click on the uninstalled APP's icon in Launchpad, it will show a question mark
 #    3. if the app starts normally:
 #        1. right click on the running app's icon in Dock, select "Options" -> "Show in Finder" and delete it
 #    4. hold down the Option key, a `x` button will appear on the icon, click it to remove the icon
-#
-##########################################################################
 
 { pkgs, ... }:
 
