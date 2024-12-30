4d5ec812b1ae1e96cd0fbe151d8c73a31e88331e
Zexin Yuan <git@yzx9.xyz>
2024-12-30 11:41:21 +0800

    feat(home/gui): udpate kitty configuration

--- home/gui/kitty.nix
+++ home/gui/kitty.nix
@@ -18,8 +18,9 @@ in
         };
         settings = {
           hide_window_decorations = if pkgs.stdenvNoCC.hostPlatform.isDarwin then "titlebar-only" else "yes";
-          window_margin_width = if pkgs.stdenvNoCC.hostPlatform.isDarwin then 5 else 0;
+          window_margin_width = 5;
           background_opacity = 0.75;
+          macos_show_window_title_in = "window";
 
           # Note that disabling the read confirmation is this akes a security risk
           clipboard_control = "write-clipboard write-primary read-clipboard read-primary";
