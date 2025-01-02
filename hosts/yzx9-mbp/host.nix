62770522bc57b711446c81867f0242a8a7e60dfc
Zexin Yuan <git@yzx9.xyz>
2025-01-02 14:36:21 +0800

    feat(hosts/yzx9-mbp): udpate apps

--- hosts/yzx9-mbp/host.nix
+++ hosts/yzx9-mbp/host.nix
@@ -16,14 +16,14 @@
       "krita"
       "inkscape"
 
-      # game
-      "steam"
-
       # misc
       "logitech-options" # mouse
       "canon-ufrii-driver" # printer
+      "wechat" # chat
+      "steam" # game
       "vmware-fusion" # virtual machine
       "vial" # keyboard
+      "bambu-studio" # 3d ptinter
     ];
   };
 }
