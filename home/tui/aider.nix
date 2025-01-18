b16aac984d2279be151b038c7e95961b720aeaf4
Zexin Yuan <git@yzx9.xyz>
2025-01-18 12:07:31 +0800

    fix(home/tui): inject secrets in runtime

--- home/tui/aider.nix
+++ home/tui/aider.nix
@@ -8,33 +8,25 @@
 let
   toYAML = lib.generators.toYAML { };
   configFile = ".aider.conf.yml";
-  placeholder = "@DEEPSEEK_API_KEY@";
 in
 lib.mkIf config.purpose.daily {
   home.packages = [
-    pkgs.aider-chat
-    pkgs.ruff
+    # API Keys and settings
+    # NOTE: inject api_key in runtime due to the limitation of agnix
+    (pkgs.writeShellScriptBin "aider" ''
+      secretFile="${config.age.secrets.deepseek-api-key.path}"
+      if [ -f "$secretFile" ]; then
+          export DEEPSEEK_API_KEY="$(cat $secretFile)"
+      else
+          echo "Warning: Secret file $secretFile does not exist, no api_key injected."
+      fi
+
+      echo $DEEPSEEK_API_KEY
+      ${lib.getExe pkgs.aider-chat}
+    '')
   ];
 
   age.secrets."deepseek-api-key".file = ../../secrets/deepseek-api-key.age;
-
-  home.activation = {
-    removeAiderConfig = lib.hm.dag.entryBefore [ "checkLinkTargets" ] ''
-      rm -f ${config.home.homeDirectory}/${configFile}
-    '';
-
-    fillAiderSecret = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      secretFile="${config.age.secrets.deepseek-api-key.path}"
-      if [ -f "$secretFile" ]; then
-          secret="$(cat "$secretFile")"
-          configFile="${config.home.homeDirectory}/${configFile}"
-          verboseEcho "Replacing placeholder with secret in config file: $configFile"
-          run ${pkgs.gnused}/bin/sed -i "s#${placeholder}#$secret#" "$configFile"
-      else
-          verboseEcho "Warning: Secret file $secretFile does not exist. Skipping placeholder replacement."
-      fi
-    '';
-  };
 
   home.file.${configFile}.text = toYAML {
     #############
@@ -42,14 +34,6 @@ lib.mkIf config.purpose.daily {
 
     # Specify the model to use for the main chat
     model = "deepseek/deepseek-chat";
-
-    ########################
-    # API Keys and settings:
-
-    ## Set an API key for a provider (eg: --api-key provider=<key> sets PROVIDER_API_KEY=<key>)
-    #api-key: xxx
-    ## Specify multiple values like this:
-    api-key = "deepseek=${placeholder}";
 
     ###############
     # Git settings:
@@ -67,7 +51,7 @@ lib.mkIf config.purpose.daily {
     #lint-cmd: xxx
     # Specify multiple values like this:
     lint-cmd = [
-      "python: ruff check --fix"
+      "python: ${lib.getExe pkgs.ruff} check --fix"
     ];
   };
 }
