a41d97bf8212144ad5de6ba5f155f29ad9f6daed
Zexin Yuan <git@yzx9.xyz>
2025-01-19 17:19:12 +0800

    refactor: update substituters

--- flake.nix
+++ flake.nix
@@ -3,16 +3,18 @@
 
   # the nixConfig here only affects the flake itself, not the system configuration!
   nixConfig = {
+    # NOTE: don't forget to update `module/_shared/nix-core.nix`
     extra-substituters = [
       # Query the mirror of USTC first, and then the official cache.
       "https://mirrors.ustc.edu.cn/nix-channels/store"
-      "https://cache.nixos.org"
       "https://nix-community.cachix.org"
+      # "https://cache.nixos.org"
     ];
 
+    # NOTE: don't forget to update `module/_shared/nix-core.nix`
     extra-trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      # "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
   };
 
--- modules/_shared/nix-core.nix
+++ modules/_shared/nix-core.nix
@@ -38,11 +38,15 @@ in
   nix.settings = {
     # substituers that will be considered before the official ones (https://cache.nixos.org)
     substituters = [
-      "https://mirror.sjtu.edu.cn/nix-channels/store"
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://nix-community.cachix.org"
+      #"https://cache.nixos.org"
     ];
 
-    trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      #"cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    ];
 
     builders-use-substitutes = true;
 
