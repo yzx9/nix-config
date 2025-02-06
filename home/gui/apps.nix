5a3011a7dbe344728a2e2171e7f4a3bb6d93b228
Zexin Yuan <git@yzx9.xyz>
2025-02-06 20:48:09 +0800

    style: simplify code

--- home/gui/apps.nix
+++ home/gui/apps.nix
@@ -8,31 +8,24 @@
 
 let
   inherit (config) vars purpose;
-  inherit (pkgs.stdenvNoCC.hostPlatform) isLinux isDarwin isAarch64;
+  inherit (pkgs.stdenvNoCC.hostPlatform) isLinux isDarwin;
 in
-{
-  config = lib.mkIf purpose.gui {
-    # Allow unfree packages
-    # nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ ];
+lib.mkIf purpose.gui {
+  # Allow unfree packages
+  # nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ ];
 
-    # The home.packages option allows you to install Nix packages into your
-    # environment.
-    home.packages = lib.optionals purpose.daily (
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages =
+    lib.optional isLinux pkgs.firefox # browser, installed using homebrew in darwin
+    ++ lib.optional isDarwin pkgs.stats # macOS system monitor in your menu bar
+    # daily only
+    ++ lib.optionals purpose.daily (
       [
         pkgs.dbeaver-bin # SQL client
+        pkgs.logseq
       ]
-      ++ lib.optionals (!(isLinux && isAarch64)) [
-        pkgs.logseq # broken in aarch64-linux
-      ]
-      # linux only
-      ++ lib.optionals isLinux [
-        pkgs.inkscape # SVG design, install using homebrew in darwin
-      ]
-      # darwin only
-      ++ lib.optionals isDarwin [
-        self.packages.${vars.system}.vaa3d-x
-        pkgs.stats
-      ]
+      ++ lib.optional isLinux pkgs.inkscape # SVG design, installed by homebrew in darwin
+      ++ lib.optional isDarwin self.packages.${vars.system}.vaa3d-x # only support darwin now
     );
-  };
 }
--- home/gui/fonts.nix
+++ home/gui/fonts.nix
@@ -6,21 +6,19 @@
 }:
 
 let
-  purpose = config.purpose;
+  inherit (config) purpose;
 in
-{
-  config = lib.mkIf purpose.gui {
-    fonts.fontconfig.enable = true;
+lib.mkIf purpose.gui {
+  fonts.fontconfig.enable = true;
 
-    home.packages = with pkgs; [
-      # It is sometimes useful to fine-tune packages
-      nerd-fonts.fira-code
+  home.packages = with pkgs; [
+    # It is sometimes useful to fine-tune packages
+    nerd-fonts.fira-code
 
-      cascadia-code # Cascadia Code NF added since 2404.23
+    cascadia-code # Cascadia Code NF added since 2404.23
 
-      noto-fonts
-      noto-fonts-cjk-sans
-      noto-fonts-emoji
-    ];
-  };
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
+  ];
 }
