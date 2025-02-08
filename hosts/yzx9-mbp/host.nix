cf076743e00881e633a18b132b6c20bc27b49372
Zexin Yuan <git@yzx9.xyz>
2025-02-08 16:32:34 +0800

    feat(hosts/yzx9-mbp): add brew casks

--- hosts/yzx9-mbp/host.nix
+++ hosts/yzx9-mbp/host.nix
@@ -6,12 +6,21 @@
       # science
       "zotero"
       # "master-pdf-editor"
-      "fiji"
+      "fiji" # image viewer
 
       # design
       "blender"
       "krita"
       "inkscape"
+
+      # productivity
+      "tencent-meeting" # video conference
+      "microsoft-word"
+      "microsoft-excel"
+      "microsoft-powerpoint"
+      # I wish to use these in homebrew, but the migration is not easy
+      # "microsoft-outlook"
+      # "onedrive"
 
       # misc
       "logitech-options" # mouse
