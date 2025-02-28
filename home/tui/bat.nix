eb569dba48d6920da50e0428098177c6a538227c
Zexin Yuan <git@yzx9.xyz>
2025-02-28 13:55:42 +0800

    feat(home/tui): bump bat theme

--- home/tui/bat.nix
+++ home/tui/bat.nix
@@ -3,19 +3,18 @@
 {
   programs.bat = {
     enable = true;
-    config = {
-      theme = "mocha";
-    };
-    themes = {
-      mocha = {
-        src = pkgs.fetchFromGitHub {
-          owner = "catppuccin";
-          repo = "bat";
-          rev = "d2bbee4f7e7d5bac63c054e4d8eca57954b31471";
-          hash = "sha256-x1yqPCWuoBSx/cI94eA+AWwhiSA42cLNUOFJl7qjhmw=";
-        };
-        file = "themes/Catppuccin Mocha.tmTheme";
+
+    config.theme = "mocha";
+
+    themes.mocha = {
+      src = pkgs.fetchFromGitHub {
+        owner = "catppuccin";
+        repo = "bat";
+        rev = "699f60fc8ec434574ca7451b444b880430319941";
+        hash = "sha256-6fWoCH90IGumAMc4buLRWL0N61op+AuMNN9CAR9/OdI=";
       };
+
+      file = "themes/Catppuccin Mocha.tmTheme";
     };
   };
 }
