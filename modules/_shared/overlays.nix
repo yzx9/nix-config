0ce60f78397e151b745fd928ae10eb2f0f3bb575
Zexin Yuan <git@yzx9.xyz>
2025-03-02 16:10:58 +0800

    fix: avoid use global pkgs in overlays

--- modules/_shared/overlays.nix
+++ modules/_shared/overlays.nix
@@ -1,5 +1,3 @@
-{ pkgs, lib, ... }:
-
 {
   nixpkgs.overlays = [
     (final: prev: {
@@ -8,8 +6,8 @@
         _finalAttrs: previousAttrs: {
           patches =
             previousAttrs.patches
-            ++ lib.optional pkgs.stdenv.targetPlatform.isDarwin (
-              pkgs.fetchpatch {
+            ++ prev.lib.optional prev.stdenvNoCC.targetPlatform.isDarwin (
+              prev.fetchpatch {
                 name = "fix-sme-darwin.patch";
                 url = "https://github.com/utmapp/UTM/raw/acbf2ba8cd91f382a5e163c49459406af0b462b7/patches/qemu-9.1.0-utm.patch";
                 hash = "sha256-S7DJSFD7EAzNxyQvePAo5ZZyanFrwQqQ6f2/hJkTJGA=";
