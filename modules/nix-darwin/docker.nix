7a07824aeb149f2c55ab8305a77f0a8aabe428da
Zexin Yuan <git@yzx9.xyz>
2025-04-22 20:00:14 +0800

    🎨 Simplify conditional config

--- modules/nix-darwin/docker.nix
+++ modules/nix-darwin/docker.nix
@@ -3,9 +3,7 @@
 let
   cfg = config.docker;
 in
-{
-  config = lib.mkIf cfg.enable {
-    # nixpkgs docker is breaked, see: https://github.com/nix-darwin/nix-darwin/issues/112
-    homebrew.casks = [ "docker" ];
-  };
+lib.mkIf cfg.enable {
+  # nixpkgs docker is breaked, see: https://github.com/nix-darwin/nix-darwin/issues/112
+  homebrew.casks = [ "docker" ];
 }
