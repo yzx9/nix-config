b2985dacc4e91f141350f85fef9c079c8a42511b
Zexin Yuan <git@yzx9.xyz>
2025-05-21 12:36:21 +0800

    🔐 Update

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,6 +4,7 @@ let
   system_yzx9-rpi5 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICvd29SPiaKFU1+Rsb+MXHmD3fMzaYfwBckUUAnoZKR7 root@yzx9-rpi5";
   system_yzx9-ws = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDxJAOqmvqV1hO39/E7SCzHI3xwqHfhNt4MWXKYYZ12m root@nixos";
 
+  user_cvcd-gpu0 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAS+yrwZT0ojxcrDfzy4c2TvDeLmjxwzpmN1e8LsD62B yzx@cvcd-gpu0";
   user_cvcd-gpu1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHFcwtCHSWorJ9Mmwc9gcX3G+mWDYS4nOScModXUcD05 yzx@cvcd-gpu1";
   user_yzx9-mbp = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH8z6g8A53hF7ATKNIGzbYf/rhSm6z+iyPm2nn7E4bnp yzx9@yzx9-mbp";
   user_yzx9-pie = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPfh2M3yz/T4fdLVtvxzwUoiLv9SC83vbuHViqdAcJ7U yzx9@yzx9-pie";
@@ -12,17 +13,13 @@ let
 
   root = [ user_yzx9-mbp ];
 
-  yzx9-mbp = root ++ [
-    system_yzx9-mbp
-    user_yzx9-mbp
-  ];
-
   yzx9-rpi5 = root ++ [
     system_yzx9-rpi5
     user_yzx9-rpi5
   ];
 
   all = root ++ [
+    user_cvcd-gpu0
     user_cvcd-gpu1
     user_yzx9-ws
     user_yzx9-mbp
@@ -40,17 +37,15 @@ in
   "api-key-openrouter.age".publicKeys = all;
   "api-key-siliconflow.age".publicKeys = all;
   "id-lab.pub.age".publicKeys = all;
+  "rss-pwd.age".publicKeys = all;
   "ssh-config.age".publicKeys = all;
   "xray.json.age".publicKeys = all;
 
-  # yzx9-mbp only
-  "id-auth_root.age".publicKeys = yzx9-mbp;
-
   # yzx9-rpi5 only
   "frpc_yzx9-rpi5.toml.age".publicKeys = yzx9-rpi5;
-  "rss-pwd.age".publicKeys = yzx9-rpi5;
 
   # root only
+  "id-auth_root.age".publicKeys = root;
   "id-git_root.age".publicKeys = root;
   "id-github_root.age".publicKeys = root;
   "id-lab.age".publicKeys = root;
