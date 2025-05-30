7cb5d387e6e9865924fcc63ddf0bec25d67c6aea
Zexin Yuan <git@yzx9.xyz>
2025-05-30 14:34:34 +0800

    🔐 Update

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   system_yzx9-mbp = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFJCN9HFXys1Ov8cCFHqpNJ61uVm642fPCVGVXqnw4KP";
   system_yzx9-pie = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKfB7heZylUnZIYNM+7DwETGeYVbzLvD6B0W+pRbbNr4 root@yzx9-pie";
-  system_yzx9-rpi5 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICvd29SPiaKFU1+Rsb+MXHmD3fMzaYfwBckUUAnoZKR7 root@yzx9-rpi5";
+  system_yzx9-rpi5 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEjQ6nQg6e154adDPnYj0bW9uCwE2k3rkWKwO2C18Ath root@yzx9-rpi5";
   system_yzx9-ws = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDxJAOqmvqV1hO39/E7SCzHI3xwqHfhNt4MWXKYYZ12m root@nixos";
   systems = [
     system_yzx9-mbp
@@ -14,7 +14,7 @@ let
   user_cvcd-gpu1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHFcwtCHSWorJ9Mmwc9gcX3G+mWDYS4nOScModXUcD05 yzx@cvcd-gpu1";
   user_yzx9-mbp = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH8z6g8A53hF7ATKNIGzbYf/rhSm6z+iyPm2nn7E4bnp yzx9@yzx9-mbp";
   user_yzx9-pie = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPfh2M3yz/T4fdLVtvxzwUoiLv9SC83vbuHViqdAcJ7U yzx9@yzx9-pie";
-  user_yzx9-rpi5 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM359KKo6BK2OtDCXLhw1zP6g8OE62Hmhf3v0MXcnyYG yzx9@yzx9-rpi5";
+  user_yzx9-rpi5 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKjkUOWFtgvQTxODKsRaTOBwgIpE/4lTZXSZRM2nKrNE yzx9@yzx9-rpi5";
   user_yzx9-ws = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGN854HSrlbBR4/yB0sRk9Plerh2rKn3ZqCZJULRRB8S yzx9@yzx9-ws";
   users = [
     user_cvcd-gpu0
@@ -34,7 +34,6 @@ let
   ];
 in
 {
-  "api-key.age".publicKeys = all;
   "api-key-deepseek.age".publicKeys = all;
   "api-key-gemini.age".publicKeys = all;
   "api-key-openrouter.age".publicKeys = all;
