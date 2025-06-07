e2644f6190b40209eddf8a82567ab1de60fcb7d0
Zexin Yuan <git@yzx9.xyz>
2025-06-07 23:22:26 +0800

    📝 (packages/nixvim): Update comments

--- packages/nixvim/plugins-extra/lazygit.nix
+++ packages/nixvim/plugins-extra/lazygit.nix
@@ -1,6 +1,7 @@
 # simple terminal UI for git commands
-# homepage: https://github.com/jesseduffield/lazygit
+# homepage: https://github.com/kdheepak/lazygit.nvim
 # nixvim doc: https://nix-community.github.io/nixvim/plugins/lazygit/index.html
+# upstream: https://github.com/jesseduffield/lazygit
 {
   plugins.lazygit = {
     enable = true;
