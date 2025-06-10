2aa2193fd82d2a66beee8c74139f48f3abf51f0f
Zexin Yuan <git@yzx9.xyz>
2025-06-10 21:41:27 +0800

    🐛 (nix-darwin): Fix module executable

--- modules/nix-darwin/proxy.nix
+++ modules/nix-darwin/proxy.nix
@@ -7,21 +7,18 @@
 
 let
   cfg = config.proxy;
-  pkg = pkgs.xray;
 in
 lib.mkIf cfg.selfHost.enable {
   age.secrets."xray.json".file = ../../secrets/xray.json.age;
 
   # run `launchctl kickstart -k system/org.nixos.xray`
   launchd.daemons.xray = {
-    path = [ pkg ];
+    path = [ pkgs.xray ];
+
+    # use command or script will warp script with wait4path automatically
+    command = "${lib.getExe pkgs.xray} --config ${config.age.secrets."xray.json".path}";
 
     serviceConfig = {
-      ProgramArguments = [
-        "${pkg}/bin/xray"
-        "--config"
-        config.age.secrets."xray.json".path
-      ];
       RunAtLoad = true;
       KeepAlive = true;
       # StandardOutPath = "/var/lib/xray.out";
