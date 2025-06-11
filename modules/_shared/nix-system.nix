a9006bc5821863b580ad74f729eb3dbaf4a1a48f
Zexin Yuan <git@yzx9.xyz>
2025-06-11 13:48:01 +0800

    🐛 Override registry in a multi-nixpkgs compatible way

--- modules/_shared/nix-system.nix
+++ modules/_shared/nix-system.nix
@@ -4,16 +4,19 @@
   # Custom NIX_PATH and flake registry
   # https://nixos-and-flakes.thiscute.world/best-practices/nix-path-and-flake-registry
 
-  # make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
-  nix.registry.nixpkgs.flake = inputs.nixpkgs; # For flake commands
-  nix.nixPath = [ "nixpkgs=${inputs.nixpkgs}" ]; # For legacy commands
-  nix.channel.enable = false; # remove nix-channel related tools & configs, we use flakes instead.
+  # Make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by
+  # this flake. This is set automatically by nixpkgs.lib.nixosSystem, but we
+  # need to override it here since we are using different nixpkgs for system
+  # and application in some case (e.g. rpi)
+  nixpkgs.flake.source = lib.mkForce inputs.nixpkgs;
 
-  # but NIX_PATH is still used by many useful tools, so we set it to the same value as the one used by this flake.
-  # Make `nix repl '<nixpkgs>'` use the same nixpkgs as the one used by this flake.
+  # remove nix-channel related tools & configs, we use flakes instead.
+  nix.channel.enable = false;
+
+  # but NIX_PATH is still used by many useful tools, so we set it to the same
+  # value as the one used by this flake. Make `nix repl '<nixpkgs>'` use the
+  # same nixpkgs as the one used by this flake.
   environment.etc."nix/inputs/nixpkgs".source = "${inputs.nixpkgs}";
-  # https://github.com/NixOS/nix/issues/9574
-  nix.settings.nix-path = lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
 
   # Add this flake to the NIX_PATH, so that `nix repl '<this-flake>'` works.
   nix.registry.yzx9.to = {
