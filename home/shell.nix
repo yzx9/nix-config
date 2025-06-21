4cc23a57aed8a0dd94c4162ab9ea08d52731b9a5
Zexin Yuan <git@yzx9.xyz>
2025-06-21 19:42:40 +0800

    🔧 (home): Enable catppuccin mocha theme for starship

--- home/shell.nix
+++ home/shell.nix
@@ -18,14 +18,14 @@
     ];
   };
 
-  # multi-shell multi-command argument completer
+  # A multi-shell multi-command argument completer
   programs.carapace = {
     enable = true;
     enableBashIntegration = true;
     enableZshIntegration = true;
   };
 
-  # prompt
+  # The minimal, blazing-fast, and infinitely customizable prompt for any shell!
   programs.starship = {
     enable = true;
 
@@ -36,6 +36,39 @@
       character = {
         success_symbol = "[›](bold green)";
         error_symbol = "[›](bold red)";
+        vimcmd_symbol = "[❮](subtext1)"; # For use with zsh-vi-mode
+      };
+
+      palette = "catppuccin_mocha";
+
+      # https://github.com/catppuccin/starship/blob/main/themes/mocha.toml
+      palettes.catppuccin_mocha = {
+        rosewater = "#f5e0dc";
+        flamingo = "#f2cdcd";
+        pink = "#f5c2e7";
+        mauve = "#cba6f7";
+        red = "#f38ba8";
+        maroon = "#eba0ac";
+        peach = "#fab387";
+        yellow = "#f9e2af";
+        green = "#a6e3a1";
+        teal = "#94e2d5";
+        sky = "#89dceb";
+        sapphire = "#74c7ec";
+        blue = "#89b4fa";
+        lavender = "#b4befe";
+        text = "#cdd6f4";
+        subtext1 = "#bac2de";
+        subtext0 = "#a6adc8";
+        overlay2 = "#9399b2";
+        overlay1 = "#7f849c";
+        overlay0 = "#6c7086";
+        surface2 = "#585b70";
+        surface1 = "#45475a";
+        surface0 = "#313244";
+        base = "#1e1e2e";
+        mantle = "#181825";
+        crust = "#11111b";
       };
     };
   };
