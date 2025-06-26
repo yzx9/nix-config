f4d79bce65a5de46a16b213bb8e7805a74144f4a
Zexin Yuan <git@yzx9.xyz>
2025-06-26 12:54:41 +0800

    🔧 (packages/nixvim): Update nvim-highlight-colors style

--- packages/nixvim/plugins-ui/nvim-highlight-colors.nix
+++ packages/nixvim/plugins-ui/nvim-highlight-colors.nix
@@ -1,20 +1,14 @@
 # Highlight colors for neovim
 # homepage: https://github.com/brenoprata10/nvim-highlight-colors
 # nixvim doc: https://nix-community.github.io/nixvim/plugins/highlight-colors/index.html
-{ icons, ... }:
+{ ... }:
 
 {
   plugins.highlight-colors = {
     enable = true;
 
     # https://github.com/brenoprata10/nvim-highlight-colors#options
-    settings = {
-      render = "virtual";
-      virtual_symbol = "${icons.FileModified}";
-      virtual_symbol_position = "eow";
-      virtual_symbol_prefix = " ";
-      virtual_symbol_suffix = "";
-    };
+    settings.render = "background";
   };
 
   keymaps = [
