e02ca17b42aefbbbc128e733f3688e49053eb1c4
Zexin Yuan <git@yzx9.xyz>
2025-07-18 10:11:35 +0800

    ✨ (packages/nixvim): Diasable autofmt for node_modules

--- packages/nixvim/plugins-lsp/conform.nix
+++ packages/nixvim/plugins-lsp/conform.nix
@@ -73,6 +73,11 @@ lib.mkIf config.lsp.enable {
           if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
             return
           end
+          -- Disable autoformat for files in a certain path
+          local bufname = vim.api.nvim_buf_get_name(bufnr)
+          if bufname:match("/node_modules/") then
+            return
+          end
           return { timeout_ms = 500, lsp_format = "fallback" }
         end
       '';
