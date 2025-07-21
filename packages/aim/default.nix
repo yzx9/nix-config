f6ec51d862344795fa2db9a6b82d7f17934f16a2
Zexin Yuan <git@yzx9.xyz>
2025-07-21 10:31:22 +0800

    ⬆️ (packages/aim): Upgrade aim

--- packages/aim/default.nix
+++ packages/aim/default.nix
@@ -1,22 +1,26 @@
 {
   lib,
+  stdenv,
   rustPlatform,
   fetchFromGitHub,
+  installShellFiles,
   testers,
 }:
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "aim";
-  version = "0.1.0";
+  version = "0.1.1";
 
   src = fetchFromGitHub {
     owner = "yzx9";
     repo = "aim";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-dIePBA8K65UNXcfFG2Y8kb+zd5gxtiHlF0TcNWhtSR4=";
+    hash = "sha256-IJFvb54la6YvHFwY2fMgukUcvRm1H737q6qcUpsYJa0=";
   };
 
-  cargoHash = "sha256-jP/CaH6lAsFXqTHGQI/dgwLCVjXJ8yQAH0FzRNhjA94=";
+  cargoHash = "sha256-nL9sZvrVLSqZxTKaZZJE1pjGSEIDf1oZv5kFA+KuCXA=";
+
+  nativeBuildInputs = [ installShellFiles ];
 
   passthru.tests = {
     version = testers.version {
@@ -25,8 +29,15 @@ rustPlatform.buildRustPackage (finalAttrs: {
     };
   };
 
+  postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+    installShellCompletion --cmd aim \
+      --bash <($out/bin/aim generate completion bash) \
+      --fish <($out/bin/aim generate completion fish) \
+      --zsh <($out/bin/aim generate completion zsh)
+  '';
+
   meta = {
-    description = "An Information Manager";
+    description = " Analyze. Interact. Manage Your Time";
     license = lib.licenses.asl20;
     platforms = lib.platforms.all;
     mainProgram = "aim";
