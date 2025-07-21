317bd8e2f2e3d724e791999c784567106e8b15d3
Zexin Yuan <git@yzx9.xyz>
2025-07-21 16:28:19 +0800

    ♻️ (yzx9-rpi5): Tweak freshrss and add comment

--- hosts/yzx9-rpi5/freshrss.nix
+++ hosts/yzx9-rpi5/freshrss.nix
@@ -4,6 +4,29 @@ let
   inherit (config) vars;
   baseUrl = "10.1.1.1";
   vhost = "freshrss";
+
+  # Custom configuration for FreshRSS.
+  # Only used before the first run, edit `/var/lib/freshrss/config.php after the install process is completed
+  configCustom = pkgs.writeText "freshrss-config.custom.php" ''
+    <?php
+
+    return [
+      'curl_options' => [
+        # Options to use a proxy for retrieving feeds.
+        CURLOPT_PROXYTYPE => CURLPROXY_HTTP,
+        CURLOPT_PROXY => '127.0.0.1',
+        CURLOPT_PROXYPORT => ${builtins.toString config.proxy.selfHost.httpProxyPublicPort},
+      ]
+    ];
+  '';
+
+  freshrssWithProxy = pkgs.freshrss.overrideAttrs (prev: {
+    postInstall =
+      (prev.postInstall or "")
+      + ''
+        cp ${configCustom} $out/config.custom.php
+      '';
+  });
 in
 {
   age.secrets."freshrss-pwd" = {
@@ -15,31 +38,7 @@ in
   services.freshrss = {
     enable = true;
 
-    package = pkgs.freshrss.overrideAttrs (
-      previousAttrs:
-
-      let
-        configCustom = pkgs.writeText "config.custom.php" ''
-          <?php
-
-          return [
-            'curl_options' => [
-              # Options to use a proxy for retrieving feeds.
-              CURLOPT_PROXYTYPE => CURLPROXY_HTTP,
-              CURLOPT_PROXY => '127.0.0.1',
-              CURLOPT_PROXYPORT => ${builtins.toString config.proxy.selfHost.httpProxyPublicPort},
-            ]
-          ];
-        '';
-      in
-      {
-        postInstall =
-          (previousAttrs.postInstall or "")
-          + ''
-            cp ${configCustom} $out/data/config.custom.php
-          '';
-      }
-    );
+    package = freshrssWithProxy;
 
     virtualHost = vhost;
     baseUrl = baseUrl;
