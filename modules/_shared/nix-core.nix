8f152873b9cde74b7bd6365bf46aff23b10b8b20
Zexin Yuan <git@yzx9.xyz>
2025-10-12 22:45:30 +0800

    ✨ Add trusted cache

--- modules/_shared/nix-core.nix
+++ modules/_shared/nix-core.nix
@@ -24,13 +24,15 @@
       "https://cache.nixos.org"
 
       "https://nix-community.cachix.org"
+      "https://yzx9.cachix.org"
       "https://nixos-raspberrypi.cachix.org"
     ];
 
     trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "yzx9.cachix.org-1:h2efDUniPK7YZAKoWZUbKH9nnxsawcKXqRjAYGkNwig="
     ];
 
     builders-use-substitutes = true;
