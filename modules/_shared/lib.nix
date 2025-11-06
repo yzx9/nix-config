7d349478accacb47fb7886a3d660395f7d3b4f75
Zexin Yuan <git@yzx9.xyz>
2025-11-06 21:26:14 +0800

    ⚡️ Reduce substituter in non-rpi system

--- modules/_shared/lib.nix
+++ modules/_shared/lib.nix
@@ -52,6 +52,16 @@ in
           # Rpi use custom bootloader
           boot.loader.systemd-boot.enable = false;
           boot.loader.efi.canTouchEfiVariables = false;
+
+          nix.settings = {
+            substituters = [
+              "https://nixos-raspberrypi.cachix.org"
+            ];
+
+            trusted-public-keys = [
+              "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+            ];
+          };
         }
       ];
       specialArgs = specialArgs // {
