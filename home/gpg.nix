4e99d0beacb2113d5070579f66dbbbd5ad74e51f
Zexin Yuan <git@yzx9.xyz>
2025-11-07 17:32:29 +0800

    ♻️ (home): Use service.gpg-agent

--- home/gpg.nix
+++ home/gpg.nix
@@ -5,30 +5,29 @@
   ...
 }:
 
-let
-  pinentry_mac = config.purpose.gui && pkgs.stdenvNoCC.hostPlatform.isDarwin;
-in
-lib.mkIf config.purpose.daily {
+{
   programs.gpg = {
-    enable = true;
+    enable = config.purpose.daily;
+
     settings = {
       keyid-format = "0xlong";
       with-fingerprint = true;
     };
   };
 
-  home.packages = lib.optional pinentry_mac pkgs.pinentry_mac;
+  services.gpg-agent = {
+    enable = config.programs.gpg.enable;
 
-  # services.gpg-agent is broken in darwin, see: https://github.com/nix-community/home-manager/issues/3864
-  home.file.".gnupg/gpg-agent.conf".text = ''
-    default-cache-ttl 60480000
-    max-cache-ttl 60480000
+    defaultCacheTtl = 60480000;
+    maxCacheTtl = 60480000;
 
-    enable-ssh-support
-    default-cache-ttl-ssh 60480000
-    max-cache-ttl-ssh 60480000
-  ''
-  + lib.optionalString pinentry_mac ''
-    pinentry-program ${lib.getExe pkgs.pinentry_mac}
-  '';
+    enableSshSupport = true;
+    defaultCacheTtlSsh = 60480000;
+    maxCacheTtlSsh = 60480000;
+
+    pinentry = lib.mkIf (config.purpose.gui && pkgs.stdenvNoCC.hostPlatform.isDarwin) {
+      package = pkgs.pinentry_mac;
+      program = "pinentry-mac";
+    };
+  };
 }
