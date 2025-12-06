f4ce7295e5e4afc4f4aafa21cc207675a0b548ab
Zexin Yuan <git@yzx9.xyz>
2025-12-06 16:09:18 +0800

    ✨ (packages/nixvim): Enable catppuccin bufferline integration

--- packages/nixvim/plugins-ui/bufferline.nix
+++ packages/nixvim/plugins-ui/bufferline.nix
@@ -2,11 +2,24 @@
 # homepage: https://github.com/akinsho/bufferline.nvim
 # nixvim doc: https://nix-community.github.io/nixvim/plugins/bufferline/index.html
 {
-  plugins.bufferline.enable = true;
+  plugins.bufferline = {
+    enable = true;
 
-  # # Enable catppuccin colors
-  # # https://github.com/catppuccin/nvim/blob/main/lua/catppuccin/groups/integrations/bufferline.lua
-  # colorschemes.catppuccin.settings.integrations.bufferline = true;
+    settings.highlights.__raw = ''require("catppuccin.special.bufferline").get_theme()'';
+
+    lazyLoad.settings.lazy = true; # Lazy load manually
+  };
+
+  # Enable catppuccin colors
+  colorschemes.catppuccin.lazyLoad.settings.after.__raw = ''
+    function()
+      -- local mocha = require("catppuccin.palettes").get_palette "mocha"
+      -- bufferline.setup {
+      --   highlights = require("catppuccin.special.bufferline").get_theme()
+      -- }
+      require('lz.n').trigger_load("bufferline.nvim")
+    end
+  '';
 
   keymaps = [
     {
@@ -17,6 +30,7 @@
         desc = "Cycle to previous buffer";
       };
     }
+
     {
       mode = "n";
       key = "<S-l>";
