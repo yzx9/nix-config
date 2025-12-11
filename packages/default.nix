5512b093292597976a67df7af25467c73e80c15f
Zexin Yuan <git@yzx9.xyz>
2025-12-11 09:47:45 +0800

    🔥 (packages/macism): Drop

--- packages/default.nix
+++ packages/default.nix
@@ -11,39 +11,23 @@ system:
 let
   pkgs = nixpkgs.legacyPackages.${system}.extend self.overlays.default;
 
+  catppuccin-bat = pkgs.callPackage ./catppuccin-bat { };
+
   nixvim' = import ./nixvim {
     inherit pkgs;
     nixvim = nixvim.legacyPackages.${system};
   };
-
-  all_platforms =
-    let
-      catppuccin-bat = pkgs.callPackage ./catppuccin-bat { };
-    in
-    {
-      inherit catppuccin-bat;
-      catppuccin-yazi-flavor = pkgs.callPackage ./catppuccin-yazi-flavor { inherit catppuccin-bat; };
-
-      aim = aim.packages.${system}.aim;
-
-      pmp-library = pkgs.callPackage ./pmp-library { };
-
-      nixvim = nixvim';
-      nixvim-lsp = nixvim'.extend { lsp.enable = true; };
-    };
-
-  darwin = {
-    macism = pkgs.callPackage ./macism { };
-
-    vaa3d-x = pkgs.callPackage ./vaa3d-x { };
-  };
 in
-if
-  (builtins.elem system [
-    "x86_64-darwin"
-    "aarch64-darwin"
-  ])
-then
-  darwin // all_platforms
-else
-  all_platforms
+{
+  inherit catppuccin-bat;
+  catppuccin-yazi-flavor = pkgs.callPackage ./catppuccin-yazi-flavor { inherit catppuccin-bat; };
+
+  aim = aim.packages.${system}.aim;
+
+  nixvim = nixvim';
+  nixvim-lsp = nixvim'.extend { lsp.enable = true; };
+
+  pmp-library = pkgs.callPackage ./pmp-library { };
+
+  vaa3d-x = pkgs.callPackage ./vaa3d-x { };
+}
