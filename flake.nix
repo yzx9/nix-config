a6d53a35a978fa8b08fc29a94700252ed3114bd8
Zexin Yuan <git@yzx9.xyz>
2026-02-24 12:00:53 +0800

    ✨ Bump inputs and switch to upstream nixpkgs for rpi

--- flake.nix
+++ flake.nix
@@ -12,13 +12,13 @@
 
       # "https://nix-community.cachix.org"
       "https://yzx9.cachix.org"
-      "https://nixos-raspberrypi.cachix.org"
+      # "https://nixos-raspberrypi.cachix.org"
     ];
 
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+      # "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
       "yzx9.cachix.org-1:h2efDUniPK7YZAKoWZUbKH9nnxsawcKXqRjAYGkNwig="
     ];
   };
@@ -39,8 +39,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # NOTE: use the modified nixpkgs
-    nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi";
+    nixos-raspberrypi = {
+      url = "github:nvmd/nixos-raspberrypi";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # Manage a user environment using Nix
     home-manager = {
