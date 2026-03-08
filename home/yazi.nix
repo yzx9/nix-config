f72872c071a00da3217d3e3e92aa3850bdbfc8cb
Zexin Yuan <git@yzx9.xyz>
2026-03-08 17:50:48 +0800

    🔧 (home): Update yazi plugins

--- home/yazi.nix
+++ home/yazi.nix
@@ -25,7 +25,10 @@
       };
 
       initLua = ''
-        require("git"):setup()
+        require("git"):setup {
+          -- Order of status signs showing in the linemode
+          order = 1500,
+        }
       '';
 
       settings = {
