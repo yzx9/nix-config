7b8bcbdceb72469dab9a89cfc60d76e437d13101
Zexin Yuan <git@yzx9.xyz>
2026-03-20 11:18:55 +0800

    🐛 (nixos): Set networking with http prefix

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -21,7 +21,7 @@ in
 lib.mkMerge [
   (lib.mkIf (cfg.httpProxy != null) {
     # Configure network proxy if necessary
-    networking.proxy.default = cfg.httpProxy;
+    networking.proxy.default = "http://${cfg.httpProxy}";
     networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
   })
 
