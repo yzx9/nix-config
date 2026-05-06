a41a1dfa1f5992f791502c389ce614f0b632122d
Zexin Yuan <git@yzx9.xyz>
2026-05-06 15:33:41 +0800

    ✨ (nix-darwin): Disable sandbox temorarily

--- modules/nix-darwin/nix.nix
+++ modules/nix-darwin/nix.nix
@@ -2,6 +2,6 @@
   # set up a launchd service to optimize the store
   nix.optimise.automatic = true;
 
-  # Enable sandboxing for Nix builds
-  nix.settings.sandbox = true;
+  ## Enable sandboxing for Nix builds
+  # nix.settings.sandbox = true;
 }
