dd97d151ad4d39cd647933c57a323a98f06e4090
Zexin Yuan <git@yzx9.xyz>
2026-05-06 15:34:37 +0800

    🐛 (overlays): Skip check for fastmcp

--- overlays/python-packages.nix
+++ overlays/python-packages.nix
@@ -1,9 +1,26 @@
 _inputs:
 
-final: prev: {
-  python3Packages = prev.python3Packages.overrideScope (
-    final': prev': {
+final: prev:
+
+let
+  inherit (prev) lib python3Packages;
+in
+{
+  python3Packages = python3Packages.overrideScope (
+    final': prev':
+
+    {
       pyzotero = prev'.toPythonModule (prev.python3Packages.callPackage ../packages/pyzotero { });
     }
+    // (lib.optionalAttrs prev.stdenv.hostPlatform.isAarch64 {
+      # lupa depends on luajit, which doesn't support aarch64.
+      # Provide a dummy so fastmcp (which has lupa in nativeCheckInputs) can evaluate.
+      lupa = prev'.toPythonModule (prev.runCommand "lupa-dummy" { } "mkdir $out");
+
+      fastmcp = prev'.fastmcp.overrideAttrs {
+        nativeCheckInputs = [ ];
+        doCheck = false;
+      };
+    })
   );
 }
