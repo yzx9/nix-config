5674a930fbe8fbf75954dfd3bca64dddef533851
Zexin Yuan <git@yzx9.xyz>
2026-07-28 15:59:40 +0800

    refactor(overlays/python-pandas-stubs): drop

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,7 +10,6 @@ let
     nur = import ./nur.nix inputs;
     packages = import ./packages.nix inputs;
     python-fastmcp = import ./python-fastmcp.nix inputs;
-    python-pandas-stubs = import ./python-pandas-stubs.nix inputs;
     stats = import ./stats.nix inputs;
     terminal-notifier = import ./terminal-notifier.nix inputs;
     zotero = import ./zotero.nix inputs;
