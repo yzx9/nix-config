d526678be69171c0c18ea57abb657aed27c26397
Zexin Yuan <git@yzx9.xyz>
2026-09-21 12:39:55 +0800

    chore: use channels.nixos.org tarball in flake input

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
   # This is the standard format for flake.nix. `inputs` are the dependencies of the flake,
   # Each item in `inputs` will be passed as a parameter to the `outputs` function after being pulled and built.
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.zst";
     systems.url = "github:nix-systems/default";
 
     nur = {
